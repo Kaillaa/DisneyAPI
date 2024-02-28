38d131c36872b16e7ac17104868c43d18263ab27
Kaillaa <kaillaa.medeiroos@gmail.com>
2024-02-28 12:30:10 -0300

    New update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('scroll', window.scrollY > 550)
+=======
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('scroll', window.scrollY > 550)
+>>>>>>> 2ef5a22 (first)
 })
